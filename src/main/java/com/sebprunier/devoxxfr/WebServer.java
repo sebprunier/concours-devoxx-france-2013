2a1cf92341d04397ad3be4714b21c8718cf563e1
sebprunier <sebastien.prunier@gmail.com>
2013-01-10 00:51:16 +0100

    enonce

--- src/main/java/com/sebprunier/devoxxfr/WebServer.java
+++ src/main/java/com/sebprunier/devoxxfr/WebServer.java
@@ -1,12 +1,12 @@
 package com.sebprunier.devoxxfr;
 
-import com.google.common.base.Objects;
 import org.mortbay.jetty.Server;
 import org.mortbay.jetty.handler.AbstractHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
@@ -45,22 +45,27 @@ public class WebServer {
             ANSWERS.put("Es tu heureux de participer(OUI/NON)", "OUI");
             ANSWERS.put("Es tu pret a recevoir une enonce au format markdown par http post(OUI/NON)", "OUI");
             ANSWERS.put("Est ce que tu reponds toujours oui(OUI/NON)", "NON");
+            ANSWERS.put("As tu bien recu le premier enonce(OUI/NON)", "NON");
         }
 
         @Override
         public void handle(String target, HttpServletRequest request, HttpServletResponse response, int dispatch) throws IOException, ServletException {
             String question = request.getParameter(QUERY_PARAMETER);
-            String answer = Objects.firstNonNull(ANSWERS.get(question), "Bad question ...");
+            String answer = ANSWERS.get(question);
+            if (answer == null) {
+                answer = "Bad question ...";
+                System.out.println("Unknown request : " + request.toString());
+            }
             response.setContentType("text/plain;charset=utf-8");
             response.setStatus(HttpServletResponse.SC_OK);
             PrintWriter writer = response.getWriter();
             writer.println(answer);
             writer.close();
         }
-
     }
 
     public static void main(String[] args) throws Exception {
+//        int port = 8888;
         int port = Integer.parseInt(System.getenv("PORT"));
         WebServer webServer = new WebServer(port);
         webServer.startAndWait();
