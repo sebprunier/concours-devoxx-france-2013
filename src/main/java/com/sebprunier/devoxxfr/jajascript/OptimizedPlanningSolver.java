afca3a6406292ce40e9b4722617b9004442c5c20
sebprunier <sebastien.prunier@gmail.com>
2013-01-17 23:15:42 +0100

    remove recursive algorithm

--- src/main/java/com/sebprunier/devoxxfr/jajascript/OptimizedPlanningSolver.java
+++ src/main/java/com/sebprunier/devoxxfr/jajascript/OptimizedPlanningSolver.java
@@ -1,6 +1,11 @@
 package com.sebprunier.devoxxfr.jajascript;
 
+import com.google.common.base.Function;
+import com.google.common.collect.Lists;
+
+import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 
 public class OptimizedPlanningSolver {
 
@@ -8,10 +13,13 @@ public class OptimizedPlanningSolver {
 
     private List<FlightDemand> sortedDemands;
 
+    private Integer optimizedGain = 0;
+
+    private String optimizedPath = "";
+
     public OptimizedPlanningSolver(List<FlightDemand> sortedDemands) {
         super();
         this.sortedDemands = sortedDemands;
-        this.optimizedPlanning = new FlightPlanning();
     }
 
     public FlightPlanning getOptimizedPlanning() {
@@ -19,24 +27,39 @@ public class OptimizedPlanningSolver {
     }
 
     public void solve() {
-        solvePartialPlanning(0, 0, new FlightPlanning());
-    }
-
-    private void solvePartialPlanning(Integer startTime, Integer demandIndex, FlightPlanning currentPlanning) {
-        if (demandIndex >= sortedDemands.size()) {
-            optimizedPlanning = max(optimizedPlanning, currentPlanning);
-        } else {
-            // Try with next demand
-            FlightDemand demand = sortedDemands.get(demandIndex);
-            FlightPlanning newFlightPlanning = new FlightPlanning(currentPlanning);
-            newFlightPlanning.setGain(newFlightPlanning.getGain() + demand.getPrice());
-            newFlightPlanning.getPath().add(demand.getFlightName());
-            solvePartialPlanning(startTime + demand.getFlightDuration(),
-                    determineNextDemandIndex(demandIndex, demand.getDepartureTime() + demand.getFlightDuration()),
-                    newFlightPlanning);
-            // Ignore next demand
-            solvePartialPlanning(startTime, demandIndex + 1, new FlightPlanning(currentPlanning));
+        Stack<SolvingContext> stack = new Stack<SolvingContext>();
+        stack.push(new SolvingContext(0, 0, 0, ""));
+        while (!stack.isEmpty()) {
+            SolvingContext context = stack.pop();
+            Integer demandIndex = context.getDemandIndex();
+            if (demandIndex >= sortedDemands.size()) {
+                //optimizedPlanning = max(optimizedPlanning, currentPlanning);
+                if (context.getGain() > optimizedGain) {
+                    optimizedGain = context.getGain();
+                    optimizedPath = context.getPath();
+                }
+            } else {
+                // Try with next demand
+                FlightDemand demand = sortedDemands.get(demandIndex);
+                stack.push(new SolvingContext(context.getStartTime()+ demand.getFlightDuration(),
+                        determineNextDemandIndex(demandIndex, demand.getDepartureTime() + demand.getFlightDuration()),
+                        context.getGain() + demand.getPrice(),
+                        context.getPath() + demandIndex + "|"));
+                // Ignore next demand
+                stack.push(new SolvingContext(context.getStartTime(), demandIndex + 1, context.getGain(), context.getPath()));
+            }
+            context = null;
         }
+        optimizedPlanning = new FlightPlanning();
+        optimizedPlanning.setGain(optimizedGain);
+        String[] pathElements = optimizedPath.split("\\|");
+        optimizedPlanning.setPath(Lists.transform(Arrays.asList(pathElements), new Function<String, String>() {
+            @Override
+            public String apply(String s) {
+                return sortedDemands.get(Integer.parseInt(s)).getFlightName();
+            }
+        }));
+        stack = null;
     }
 
     private Integer determineNextDemandIndex(Integer demandIndex, Integer nextAvailableDeparture) {
@@ -51,7 +74,33 @@ public class OptimizedPlanningSolver {
         return nextDemandIndex;
     }
 
-    private FlightPlanning max(FlightPlanning p1, FlightPlanning p2) {
-        return p1.getGain() > p2.getGain() ? p1 : p2;
+    private class SolvingContext {
+        private Integer startTime;
+        private Integer demandIndex;
+        private Integer gain;
+        private String path;
+
+        private SolvingContext(Integer startTime, Integer demandIndex, Integer gain, String path) {
+            this.startTime = startTime;
+            this.demandIndex = demandIndex;
+            this.gain = gain;
+            this.path = path;
+        }
+
+        public Integer getStartTime() {
+            return startTime;
+        }
+
+        public Integer getDemandIndex() {
+            return demandIndex;
+        }
+
+        public Integer getGain() {
+            return gain;
+        }
+
+        public String getPath() {
+            return path;
+        }
     }
 }
