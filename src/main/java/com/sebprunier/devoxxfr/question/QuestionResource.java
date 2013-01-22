61f7a767ceb32214938aa3277b934586bb6bdec2
sebprunier <sebastien.prunier@gmail.com>
2013-01-22 23:07:48 +0100

    Fixing issue 2

--- src/main/java/com/sebprunier/devoxxfr/question/QuestionResource.java
+++ src/main/java/com/sebprunier/devoxxfr/question/QuestionResource.java
@@ -2,6 +2,8 @@ package com.sebprunier.devoxxfr.question;
 
 import groovy.lang.GroovyShell;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -28,9 +30,6 @@ public class QuestionResource implements Resource {
         ANSWERS.put("As tu passe une bonne nuit malgre les bugs de l etape precedente(PAS_TOP/BOF/QUELS_BUGS)", "QUELS_BUGS");
         ANSWERS.put("As tu bien recu le second enonce(OUI/NON)", "OUI");
         ANSWERS.put("As tu copie le code de ndeloof(OUI/NON/JE_SUIS_NICOLAS)", "NON");
-
-        // FIXME Should not hard-code this answer ...
-        ANSWERS.put("((1,1 2) 3,14 4 (5 6 7) (8 9 10)*4267387833344334647677634)/2*553344300034334349999000", "31878018903828899277492024491376690701584023926880");
     }
 
     @Override
@@ -44,8 +43,7 @@ public class QuestionResource implements Resource {
                 String expr = question.replaceAll(" ", "+");
                 expr = expr.replaceAll(",", "\\.");
                 Object res = new GroovyShell().evaluate(expr);
-                answer = String.valueOf(res);
-                answer = new DecimalFormat("0.#####").format(Double.valueOf(answer));
+                answer = new DecimalFormat("0.#####").format(res);
                 answer = answer.replaceAll("\\.", ",");
             } catch (Exception e) {
                 e.printStackTrace(System.err);
